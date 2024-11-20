306d5ceef2a667eab3421b63aab21c52a0a447ab
Soham Desai <150273704+ItsMeSoham6508@users.noreply.github.com>
2024-11-20 10:19:21 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -18,6 +18,38 @@ var vid_list = [
     {
         "link": "https://www.youtube.com/watch?v=3V4KtC1rN-Y",
         "title": "Learn the Basic Shapes in Spanish: Circle, Square & Triangle (Círculo, Cuadrado y Triángulo)"
+    },
+    {
+       "link": "https://www.youtube.com/watch?v=-sX4CmGeZrc",
+       "title": "Farm Animals in ENGLISH and SPANISH! | Jack Hartmann" 
+    },
+    {
+       "link": "https://www.youtube.com/watch?v=0218OFXQtXM",
+       "title": "Classroom Objects song 1" 
+    },
+    {
+       "link": "https://www.youtube.com/watch?v=41cJ0mqWses",
+       "title": "School Supplies Song" 
+    },
+    {
+       "link": "https://www.youtube.com/watch?v=3JZi2oDvPs4",
+       "title": "Big, Big, Big | Adjectives Song for Kids" 
+    },
+    {
+       "link": "https://www.youtube.com/watch?v=YW-fP7INEmY",
+       "title": "The Opposites Song | La canción de los opuestos | Bilingual Preschool Vocabulary | Canticos" 
+    },
+    {
+       "link": "https://www.youtube.com/watch?v=I8GeA3anPdo",
+       "title": "How's the Weather Song" 
+    },
+    {
+       "link": "https://www.youtube.com/watch?v=H9ZAryfghmo",
+       "title": "Canticos | Weather the Weather / Jarabe Tapatío | Best Nursery Rhyme for Kids | Early Education" 
+    },
+    {
+       "link": "https://www.youtube.com/watch?v=jdgZFLKRZ0A",
+       "title": "Let's Get Dressed Song | Clothes Song for Kids | Kindergarten, Preschool & ESL | Fun Kids English" 
     }
 ]
 
@@ -38,18 +70,17 @@ for (let i = 1; i <= maxPaddingLevel; i++) {
 }
 
 
-document.getElementById('search-btn').onclick = function() {
-    const selectElement = document.getElementById("lesson-select");
-    const selectedValue = selectElement.value;
-    const selectedText = selectElement.options[selectElement.selectedIndex].text;
-    document.getElementById(selectedText).scrollIntoView({behavior: "smooth"})
-}
+
 
 function video_play(index) {
     const modal_title = document.getElementById('videoModalLabel');
+    const vidlink = document.getElementById('vid-link');
     modal_title.innerHTML = vid_list[index]['title'];
+    
     const iframe = document.getElementById('modal_video');
     const videoId = vid_list[index]['link'].split('v=')[1];
+    vidlink.href = vid_list[index]['link'];
+    vidlink.innerHTML = vid_list[index]['title'];
     const embedLink = `https://www.youtube.com/embed/${videoId}`;
     iframe.src = embedLink;
     console.log(modal_title.textContent, iframe);
